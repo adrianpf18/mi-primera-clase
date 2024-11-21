e2a17efef98167f2c051dce330fd75990dc56233
Adrianpf <adrianpalomofe@gmail.com>
2024-11-21 11:09:02 +0100

    Commit Inicial

---
+++ DestinoTuristico.java
@@ -0,0 +1,46 @@
+public class DestinoTuristico {
+    private String nombreDestino;
+    private int costoPromedio;
+    private boolean esPlaya;
+
+    // Constructor
+    public DestinoTuristico(String nombre, int costoParaIr) {
+        nombreDestino = nombre; 
+        costoPromedio = costoParaIr; 
+        esPlaya = true; 
+    }
+
+    // Getter para nombreDestino
+    public String getNombreDestino() {
+        return nombreDestino; 
+    }
+
+    // Getter para costoPromedio
+    public int getCostoPromedio() {
+        return costoPromedio; 
+    }
+
+    // Getter para esPlaya
+    public boolean getEsPlaya() {
+        return esPlaya; 
+    }
+
+    // Setter para nombreDestino
+    public void setNombreDestino(String nuevoNombre) {
+        nombreDestino = nuevoNombre; 
+    }
+
+    // Setter para costoPromedio
+    public void setCostoPromedio(int nuevoCoste) {
+        costoPromedio = nuevoCoste; 
+    }
+
+    // Método para alternar el valor de esPlaya
+    public void alternarPlaya() {
+        if (esPlaya == false) {
+            esPlaya = true;
+        } else {
+            esPlaya = false;
+        }
+    }
+}
